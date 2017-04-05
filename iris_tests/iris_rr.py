b9864a99ab6525b0a864e03f0273e0b59c687b05
Jorge Garcia <hegg777@gmail.com>
2017-04-04 21:22:51 -0500

    Add K evaluate heruistic

--- iris_tests/iris_rr.py
+++ iris_tests/iris_rr.py
@@ -20,12 +20,37 @@ xc = np.subtract(x, xmean)#XC
 
 xtc = xc.transpose()#Transpose of XC
 
-c = np.dot(xtc, xc)
+c = np.dot(xtc, xc) #C MATRIX, this is the eig val and eig vec matrix
 
 eig_val, eig_vec = LA.eig(c)
 
-#Selects the best eigen vectors, 85%
+# ---- Heuristic that evaluates K best ratio rules k_positions are the RR---
+# Covariance
+cc = np.cov(c)
+
+# Selects the best eigen vectors, 85%
+sum_eigen_val = np.sum(eig_val)
+eigthy = sum_eigen_val * 85 / 100 # 85% of eigenvectors sum
+
+#Function that search for the max closest number from a list
+takeClosest = lambda collection,num:max(collection,key=lambda x:(x-num))
+
+k = [] #eigenvalues that sum 85% of  the eigenvectors
+Z = []
+print eigthy
+while np.sum(k) < eigthy:
+    Z = set(eig_val) - set(k)
+    print Z
+    rest = eigthy - np.sum(k)
+    print rest
+    k.append(takeClosest(Z,rest))
+    print k
+
+k_positions = []
+for i in range(len(k)):
+    k_positions.append(eig_vec[i])
+# ---- Heuristic that evaluates K best ratio rules k_positions are the RR---
 
 
 
-pdb.set_trace()
+#pdb.set_trace()
