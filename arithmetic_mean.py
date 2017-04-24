19fe4100d865e75a99ac8bb30810d9b691cc6811
Rolando Islas <dani.islas96@gmail.com>
2017-04-23 20:02:12 -0500

    Fix accuracy percentage between original_row and predicted_row

--- arithmetic_mean.py
+++ arithmetic_mean.py
@@ -1,20 +1,21 @@
-import numpy as np
-
 def get_accuracy_percentage(original_row, predicted_row):
-    a = np.array(original_row)
-    b = np.array(predicted_row)
-    error = np.mean(a != b)
-    print error
-    error_percentage = error * 10
-    accuracy_percentage = 100 - error_percentage
-    return accuracy_percentage
+    total_percentage = 0
+    for index,value in enumerate(original_row):
+        error = abs(value - predicted_row[index])
+        error_percentage = (error * 100) / value
+        if error_percentage > 100:
+            error_percentage = 100
+        accuracy_percentage = 100 - error_percentage
+        total_percentage += accuracy_percentage
+        # print "------"
+        # print error_percentage
+    percentage_average = total_percentage / len(original_row)
+    return percentage_average
+
 
 # EXAMPLES
-# original_row = [0.8129356725834412, 6655.0, 1351.6863411946172, 4632.1839446400145]
+# CHANGE VALUES OF ARRAY TO GET DIFFERENT ACCURACY
+# original_row = [0.8029466725834412, 6655.0, 1352.6863411946172, 4732.1839446400145]
 # predicted_row = [0.8029466725834412, 6655.0, 1352.6863411946172, 4732.1839446400145]
 # a = get_accuracy_percentage(original_row, predicted_row)
 # print a
-
-# SOURCE
-# http://stackoverflow.com/questions/20402109/calculating-percentage-error-by-comparing-two-arrays
-# https://en.wikipedia.org/wiki/Arithmetic_mean
