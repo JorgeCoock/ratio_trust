bf6e2b3838bdc79fc2da2a736edbf6d7b2ca392a
Jorge Garcia <hegg777@gmail.com>
2017-05-22 22:43:49 -0500

    Add youtube rr

---
+++ youtube/youtube_rr.py
@@ -0,0 +1,23 @@
+import pdb
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import csv
+
+from main import init_main, myFloat
+
+COLUMN_RANGES_FILE_LOCATION = '/home/jorge/Documents/FIME/8/mineria/pia/youtube/'
+
+with open('/home/jorge/Documents/FIME/8/mineria/databases/shuffled/youtube.csv', 'rb') as f:
+    reader = csv.reader(f, delimiter=',')
+    log = list(reader)
+    data, headers = log[:], log[:]
+    headers = headers[0]
+    data.pop(0)
+
+dataset = []
+for index, value in enumerate(data):
+    dataset.append(value)
+
+matrix = map(myFloat, dataset)
+
+init_main(matrix, COLUMN_RANGES_FILE_LOCATION)
